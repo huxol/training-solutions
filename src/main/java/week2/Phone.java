e40b50d07540a947dc3454654bae5c5f0a04ae06
Huxol <huxol@freemail.hu>
2020-11-04 19:48:42 +0100

    Fele se megy.

--- src/main/java/week2/Phone.java
+++ src/main/java/week2/Phone.java
@@ -1,7 +1,20 @@
 package week2;
 
 public class Phone {
+    private int mem;
+    private String type;
 
-    private String Phone;
+    public Phone(String type, int mem) {
+        this.mem = mem;
+        this.type = type;
+    }
+
+    public int getMem() {
+        return mem;
+    }
+
+    public static void main(String[] args) {
+    }
 
 }
+
