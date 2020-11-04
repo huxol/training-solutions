b6c07ff941225649a2af83d4029c42572c97e270
Huxol <huxol@freemail.hu>
2020-11-04 19:23:25 +0100

    Fele se megy.

---
+++ src/main/java/week2/Product.java
@@ -0,0 +1,36 @@
+package week2;
+
+public class Product {
+
+    private String name;
+    private String code;
+
+    public Product(String name, String code) {
+        this.name = name;
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public boolean areTheyEqual(Product p) {
+        return name.equals(p.getName());
+    }
+
+    public static void main(String[] args) {
+        Product mars = new Product("Mars", "00001");
+        Product mars2 = new Product("Mars2", "01");
+        Product sportSzelet = new Product("Sport Szelet", "0120");
+        Product sportSzelet2 = new Product("Sport Szelet2", "012");
+        Product snickers = new Product("Snickers", "983001");
+
+        System.out.println(mars.getName() + " és " + sportSzelet.getName() + " azonosak? " + mars.areTheyEqual(sportSzelet));
+        System.out.println(mars.getName() + " és " + mars.getName() + " azonosak? " + mars.areTheyEqual(mars));
+        System.out.println(snickers.getName() + " és " + mars.getName() + " azonosak? " + snickers.areTheyEqual(mars));
+    }
+}
