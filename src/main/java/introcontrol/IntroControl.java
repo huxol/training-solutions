61498e21a967b1859aa600a860089e0aaed9185e
Huxol <huxol@freemail.hu>
2020-11-24 19:30:59 +0100

    Fele se megy.

---
+++ src/main/java/introcontrol/IntroControl.java
@@ -0,0 +1,14 @@
+package introcontrol;
+
+public class IntroControl {
+
+    public int subtractTenIfGreaterThanTen(int number) {
+        if (number > 10) {
+            return number - 10;
+        } else {
+            return number;
+        }
+    }
+
+
+}
