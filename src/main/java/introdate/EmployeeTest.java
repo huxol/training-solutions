6824a9437d610c0148727666f30fe53a58ad77f0
Huxol <huxol@freemail.hu>
2020-11-24 13:43:50 +0100

    Fele se megy.

---
+++ src/main/java/introdate/EmployeeTest.java
@@ -0,0 +1,11 @@
+package introdate;
+
+public class EmployeeTest {
+
+    public static void main(String[] args) {
+
+        Employee employee = new Employee(2000, 1, 11, "John Doe");
+        System.out.println(employee.getBeginEmployment());
+        System.out.println(employee.getDateOfBirth());
+    }
+}
