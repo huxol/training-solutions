4b7638b44756ee63aa1c70032aa56588086f5b54
Huxol <huxol@freemail.hu>
2020-11-24 13:49:46 +0100

    Fele se megy.

--- src/main/java/introdate/EmployeeTest.java
+++ src/main/java/introdate/EmployeeTest.java
@@ -5,6 +5,7 @@ public class EmployeeTest {
     public static void main(String[] args) {
 
         Employee employee = new Employee(2000, 1, 11, "John Doe");
+        System.out.println(employee.getName());
         System.out.println(employee.getBeginEmployment());
         System.out.println(employee.getDateOfBirth());
     }
