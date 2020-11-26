3165b57f6f29d792930ac4d75d25da3b95ca72ee
Huxol <huxol@freemail.hu>
2020-11-26 17:52:34 +0100

    Fele se megy.

---
+++ src/main/java/arrayofarrays/ArrayOfArraysMain.java
@@ -0,0 +1,16 @@
+package arrayofarrays;
+
+public class ArrayOfArraysMain {
+
+    int[][] multiplicationTable(int size) {
+        int[][] multiplicationTable = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                multiplicationTable[i][j] = (i + 1) * (j + 1);
+            }
+        }
+        return multiplicationTable;
+    }
+
+
+}
