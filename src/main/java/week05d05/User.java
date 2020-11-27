741f8b6c2c43ddea6b55b329a54881224f24e34d
Huxol <huxol@freemail.hu>
2020-11-27 19:13:58 +0100

    Fele se megy.

---
+++ src/main/java/week05d05/User.java
@@ -0,0 +1,10 @@
+package week05d05;
+
+public class User {
+}
+
+//* Készíts egy osztályt week05d05.User néven, melynek van egy firstName (String), egy lastName (String) és
+// egy email (String) mezője. Legyen ellenőrzés az User konstruktorában, mely hibát jelez,
+// ha nincs @ és . (pont) az e-mail cimben! Készíts egy metódust is getFullName névvel, mely visszaadja
+// a firstName és a lastName változókat összefűzve. Legyen közöttük egy whitespace (" ").
+//Bónusz feladat: írjunk teszteket
