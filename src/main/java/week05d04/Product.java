ad92e2e66bf8e31ca0e234ec01dd4c606189ade5
Huxol <huxol@freemail.hu>
2020-11-27 19:08:52 +0100

    Fele se megy.

---
+++ src/main/java/week05d04/Product.java
@@ -0,0 +1,16 @@
+package week05d04;
+
+public class Product {
+
+}
+
+//*Készíts egy week05d04.Product osztályt melyben egy termékről tárolod a nevét és a lejárati dátumát (LocalDate).
+// Legyen egy konstruktora, ami a nevet kapja meg illetve a lejárati dátumot három részletben int year, int month,
+// int day. (public Product(String name, int year, int month, int day))
+//A lejárati dátumot a konstruktorban állítsd össze.
+//Legyen mindkettőhöz getter metódus.
+//Készíts egy week05d04.Store osztályt, melyben van egy Productokat tároló lista. Legyen benne egy
+// addProduct(Product) metódus.
+//Bónusz: Legyen benne egy getNumberOfExpired() metódus ami visszaadja a lejárt termékek számát.
+// Használd a LocalDate.now() és az isBefore() metódusokat.
+//Tesztelj!
