f6902817f88999a243a28bb5f42201d7b6e9c60b
Huxol <huxol@freemail.hu>
2020-11-28 11:17:27 +0100

    Fele se megy.

--- src/main/java/arrays/ArraysMain.java
+++ src/main/java/arrays/ArraysMain.java
@@ -28,5 +28,29 @@ public class ArraysMain {
         return Arrays.equals(day, anotherDay);
     }
 
+    private int min(int a, int b) {
+        if (a < b) {
+            return a;
+        }
+        else {
+            return b;
+        }
+    }
+
+    public boolean sameTempValuesDayLight(double[] day, double[] anotherDay) {
+        int min = min(day.length, anotherDay.length);
+        double[] rangeOfDay = Arrays.copyOfRange(day, 0, min);
+        double[] rangeOfAnotherDays = Arrays.copyOfRange(anotherDay, 0, min);
+        return  Arrays.equals(rangeOfDay, rangeOfAnotherDays);
+    }
+
+    public  boolean wonLottery(int[] winner, int[] stake) {
+        int[] copyOfWinner = Arrays.copyOf(winner, winner.length);
+        int[] copyOfStake = Arrays.copyOf(stake, stake.length);
+        Arrays.sort(copyOfWinner);
+        Arrays.sort(copyOfStake);
+        return Arrays.equals(copyOfWinner, copyOfStake);
+    }
+
 
 }
