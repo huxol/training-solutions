c5c480e015716c0e5a7d8a77e0862b032ea88260
Huxol <huxol@freemail.hu>
2020-11-29 15:28:37 +0100

    Fele se megy.

--- src/main/java/attributes/person/Person.java
+++ src/main/java/attributes/person/Person.java
@@ -9,4 +9,34 @@ public class Person {
     private String identificationCard;
 
     private Address address;
+
+    public Person(String name, String identificationCard) {
+        this.name = name;
+        this.identificationCard = identificationCard;
+    }
+
+    public void correctData(String name, String identificationCard) {
+        this.name = name;
+        this.identificationCard = identificationCard;
+    }
+
+    public void moveTo(Address address) {
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getIdentificationCard() {
+        return identificationCard;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public String personToString() {
+        return getName() + " , " + getIdentificationCard();
+    }
 }
