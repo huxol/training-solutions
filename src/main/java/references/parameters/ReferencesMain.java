85828a8d150696a9c2740e3ab0de81b1ba0d01cf
Huxol <huxol@freemail.hu>
2020-11-29 20:17:03 +0100

    Fele se megy.

--- src/main/java/references/parameters/ReferencesMain.java
+++ src/main/java/references/parameters/ReferencesMain.java
@@ -11,6 +11,16 @@ public class ReferencesMain {
         System.out.println(person.getName() + " " + person.getAge());
         System.out.println(otherPerson.getName() + " " + otherPerson.getAge());
 
+        int number1 = 24;
+        int number2 = number1;
 
+        number2++;
+
+        System.out.println(number1);
+        System.out.println(number2);
+
+        otherPerson = new Person("Jane Doe", 33);
+        System.out.println(person.getName() + " " + person.getAge());
+        System.out.println(otherPerson.getName() + " " + otherPerson.getAge());
     }
 }
