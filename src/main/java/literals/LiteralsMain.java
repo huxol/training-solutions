dad7bbc6c6bb02caf4021bbcde6caad4ee642b91
Huxol <huxol@freemail.hu>
2020-11-30 15:26:45 +0100

    Fele se megy.

---
+++ src/main/java/literals/LiteralsMain.java
@@ -0,0 +1,25 @@
+package literals;
+
+public class LiteralsMain {
+
+    public static void main(String[] args) {
+        System.out.println(Integer.toString(1) + Integer.toString(2));
+        System.out.println("1" + "" + "2");
+        System.out.println(1 + "" + 2);
+
+        double quotient1 = 3 / 4;
+        double quotient2 = 3 / 4d;
+        double quotient3 = 3.0 / 4.0;
+        System.out.println(quotient1);
+        System.out.println(quotient2);
+        System.out.println(quotient3);
+
+        long big = 3_244_444_444L;
+        System.out.println(big);
+
+        String str = "árvíztűrőtükörfúrógép";
+        System.out.println(str);
+
+
+    }
+}
