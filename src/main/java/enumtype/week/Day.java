91828e265c5267837aef0413db8c31d0c05f8c91
Huxol <huxol@freemail.hu>
2020-11-30 16:25:17 +0100

    Fele se megy.

---
+++ src/main/java/enumtype/week/Day.java
@@ -0,0 +1,15 @@
+package enumtype.week;
+
+public enum Day {
+
+    MONDAY(DayType.WORKDAY),
+    TUESDAY(DayType.WORKDAY),
+    WEDNESDAY(DayType.WORKDAY),
+    THURSDAY(DayType.WORKDAY),
+    FRIDAY(DayType.WORKDAY),
+    SATURDAY(DayType.HOLIDAY),
+    SUNDAY(DayType.HOLIDAY),
+
+
+
+}
