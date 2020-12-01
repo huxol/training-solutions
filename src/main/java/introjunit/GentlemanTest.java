56c46c8b5711937aaae8fd8bcc20a9888ab0477d
Huxol <huxol@freemail.hu>
2020-12-01 08:31:12 +0100

    Fele se megy.

--- src/main/java/introjunit/GentlemanTest.java
+++ src/main/java/introjunit/GentlemanTest.java
@@ -1,6 +1,17 @@
 package introjunit;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class GentlemanTest {
 
+    @Test
+    public void byNormalName() {
+        String name = "John Doe";
 
+        String greeting = new Gentleman().sayHello(name);
+
+        assertEquals("Hello John Doe", greeting);
+    }
 }
