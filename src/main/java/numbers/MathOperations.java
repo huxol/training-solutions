0056fe949fb35ffce32124d8825bd8740a9cdd3a
Huxol <huxol@freemail.hu>
2020-12-01 07:53:52 +0100

    Fele se megy.

--- src/main/java/numbers/MathOperations.java
+++ src/main/java/numbers/MathOperations.java
@@ -7,7 +7,7 @@ public class MathOperations {
     public static void main(String[] args) {
 
         String problem = "3 / 8 + 7 (8 - 2)";
-        double result = 3.0 * 8.0 + 7.0 / (8.0 -2.0);
+        double result = 3.0 / 8.0 + 7.0 / (8.0 - 2.0);
 
         Scanner scanner = new Scanner(System.in);
 
@@ -17,7 +17,7 @@ public class MathOperations {
         double resultByUser = scanner.nextDouble();
 
         if (Math.abs(result - resultByUser) < 0.0001) {
-            System.out.println("Helyes eredmény!");
+            System.out.println("Helyes az eredmény!");
         } else {
             System.out.println("Hibás az eredmény!");
         }
