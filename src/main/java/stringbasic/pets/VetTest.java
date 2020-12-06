187af00753a5d21e79d45346938c233b1df87ccb
Huxol <huxol@freemail.hu>
2020-12-06 19:56:12 +0100

    Fele se megy.

--- src/main/java/stringbasic/pets/VetTest.java
+++ src/main/java/stringbasic/pets/VetTest.java
@@ -1,10 +1,12 @@
 package stringbasic.pets;
 
+
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class VetTest {
 
@@ -13,7 +15,17 @@ public class VetTest {
         Vet vet = new Vet();
 
         vet.add(new Pet("Blöki", LocalDate.of(2017,10,23), Gender.MALE, "000145"));
-        vet.add(new Pet("Szerénke", LocalDate.of(2012,4,9), Gender.FEMALE, "000145"));
+        vet.add(new Pet("Szerénke", LocalDate.of(2012,4,9), Gender.FEMALE, "000129"));
+
+        assertEquals(2, vet.getPets().size());
+    }
+
+    @Test
+    public void  testAddSame() {
+        Vet vet = new Vet();
+
+        vet.add(new Pet("Blöki", LocalDate.of(2017, 10, 23), Gender.MALE, "000145"));
+        vet.add(new Pet("Szerénke", LocalDate.of(2012, 4, 9), Gender.FEMALE, "000145"));
 
         assertEquals(1, vet.getPets().size());
     }
