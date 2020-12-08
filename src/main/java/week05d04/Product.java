fccaa40a4b960c8ea3d951725fe4a0039cd1b8ad
Huxol <huxol@freemail.hu>
2020-12-08 13:17:07 +0100

    Fele se megy.

--- src/main/java/week05d04/Product.java
+++ src/main/java/week05d04/Product.java
@@ -1,7 +1,31 @@
 package week05d04;
 
+import java.time.LocalDate;
+
 public class Product {
 
+    private String name;
+    private LocalDate expireDate;
+
+    public Product(String name, int year, int month, int day) {
+        this.name = name;
+        expireDate = LocalDate.of(year, month, day);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getExpireDate() {
+        return expireDate;
+    }
+
+    public boolean isExpired() {
+        if (expireDate.isBefore(LocalDate.now())) {
+            return true;
+        }
+        return false;
+    }
 }
 
 //*Készíts egy week05d04.Product osztályt melyben egy termékről tárolod a nevét és a lejárati dátumát (LocalDate).
