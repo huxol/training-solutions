994ff13105c5f9abf462709ffa2b7c1d95994809
Huxol <huxol@freemail.hu>
2020-12-09 12:19:24 +0100

    Fele se megy.

---
+++ src/main/java/week06d05/Biscuit.java
@@ -0,0 +1,33 @@
+package week06d05;
+
+public class Biscuit {
+
+    private BiscuitType type;
+    private  int gramAmount;
+
+    public Biscuit(BiscuitType type, int gramAmount) {
+        this.type = type;
+        this.gramAmount = gramAmount;
+    }
+
+    public BiscuitType getType() {
+        return type;
+    }
+
+    public int getGramAmount() {
+        return gramAmount;
+    }
+
+    public static Biscuit of(BiscuitType type, int gramAmount) {
+        return new Biscuit(type, gramAmount);
+    }
+}
+
+
+//* Készíts a week06d05 csomagban egy Biscuit nevű osztályt, amelynek van 2 attribútuma:
+// BiscuitType type és int gramAmount. A BiscuitType legyen felsorolásos típus a 3 kedvenc
+// kekszfajtáddal. A Biscuit nevű osztálynak legyen egy static metódusa of néven,
+// mely paraméterként egy BiscuitType-ot, és egy int gramAmount-ot vár és
+// visszaad egy új Biscuit objektumot amelyet a megadott paraméterek alapján hoz létre.
+// Bónusz feladat: konzolra írás esetén a Biscuit jelenítse meg a type és a
+// gramAmount mezőket!
