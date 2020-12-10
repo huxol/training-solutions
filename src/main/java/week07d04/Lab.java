5908912d182658a3a5711d73bc660bf5bc95133b
Huxol <huxol@freemail.hu>
2020-12-10 18:14:24 +0100

    Fele se megy.

--- src/main/java/week07d04/Lab.java
+++ src/main/java/week07d04/Lab.java
@@ -1,8 +1,43 @@
 package week07d04;
 
+import java.time.LocalDate;
+
 public class Lab {
 
+    private String title;
+    private boolean completed;
+    private LocalDate completedAt;
 
+    public Lab(String title) {
+        this.title = title;
+        this.completed = false;
+        this.completedAt = null;
+    }
+
+    public Lab(String title, LocalDate completedAt) {
+        this.title = title;
+        this.completed = true;
+        this.completedAt = completedAt;
+    }
+
+    public void complete(LocalDate localDate) {
+        this.completed = true;
+        this.completedAt = localDate;
+    }
+
+    public void complete() {
+        this.completed = true;
+        this.completedAt = LocalDate.now();
+    }
+
+    @Override
+    public String toString() {
+        return "Lab{" +
+                "title='" + title + '\'' +
+                ", completed=" + completed +
+                ", completedAt=" + completedAt +
+                '}';
+    }
 }
 
 
