762c768e2f866b385ad2f2ba65a216791452bdd7
Huxol <huxol@freemail.hu>
2020-12-15 11:49:01 +0100

    Fele se megy.

---
+++ src/main/java/week08d02/Lottery.java
@@ -0,0 +1,11 @@
+package week08d02;
+
+public class Lottery {
+
+
+}
+
+
+//* Készíts a week08d02 csomagban egy Lottery osztályt, melynek legyen egy getNumbers() metódusa,
+// ami visszaad egy listát amiben 5 véletlen szám van 1-90 között, tehát kvázi készíts egy
+// lottósorsoló programot. A sorrendre nem kell figyelni, a lényeg, hogy a számok különbözők legyenek!
