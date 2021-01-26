d2bffaf93e882e337beed0b272037790c181f659
Huxol <huxol@freemail.hu>
2021-01-26 20:30:09 +0100

    "Fele se megy."

--- src/main/java/names/NameWriter.java
+++ src/main/java/names/NameWriter.java
@@ -1,4 +1,24 @@
 package names;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+
 public class NameWriter {
+
+    private Path file;
+
+    public NameWriter(Path file){
+        this.file = file;
+    }
+
+    public void addAndWrite(String name) {
+        try (BufferedWriter bw = Files.newBufferedWriter(file, StandardOpenOption.APPEND)) {
+            bw.write(name + "\n");
+        } catch (IOException e) {
+            throw new IllegalStateException("Can't open file!", e);
+        }
+    }
 }
