d9bc40b631a92d7e0fa1883c428581449922d9fb
Huxol <huxol@freemail.hu>
2021-02-15 19:37:50 +0100

    "Fele se megy."

---
+++ src/main/java/exam03/Cruise.java
@@ -0,0 +1,38 @@
+package exam03;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cruise {
+
+    private Boat boat;
+    private LocalDate sailing;
+    private double basicPrice;
+    private List<Passenger> passengers = new ArrayList<>();
+
+    public Cruise(Boat boat, LocalDate sailing, double basicPrice) {
+        this.boat = boat;
+        this.sailing = sailing;
+        this.basicPrice = basicPrice;
+    }
+
+    public Boat getBoat() {
+        return boat;
+    }
+
+    public LocalDate getSailing() {
+        return sailing;
+    }
+
+    public double getBasicPrice() {
+        return basicPrice;
+    }
+
+    public List<Passenger> getPassengers() {
+        return passengers;
+    }
+
+    public void bookPassenger(Passenger john_doe) {
+    }
+}
--- src/main/java/exam03/Histogram.java
+++ src/main/java/exam03/Histogram.java
@@ -1,6 +1,28 @@
 package exam03;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class Histogram {
 
+    public String createHistogram(BufferedReader reader) throws IOException {
+        String line;
+        StringBuilder result = new StringBuilder();
+        while ((line = reader.readLine())  != null) {
+            result.append("*".repeat(Integer.parseInt(line)));
+            result.append("\n");
+        }
+        return result.toString();
+    }
 
+    public static void main(String[] args) {
+        try (BufferedReader reader = Files.newBufferedReader(Path.of("src/main/resources/histogram.txt"))) {
+            new FileReader().readLines(reader);
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Can not read file", ioe);
+        }
+    }
 }
